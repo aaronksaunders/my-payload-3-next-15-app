861a8197d11ce450b41e137225666c83bf9b50e6
Aaron Saunders <aaronksaunders@gmail.com>
2024-08-23 01:49:18 -0400

    initial commit

---
+++ src/app/(app)/my-page/page.tsx
@@ -0,0 +1,26 @@
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+
+export default async function MyPage() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "users",
+  });
+
+  return (
+    <div>
+      <h1>My Page</h1>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <div>
+        {data.docs.map((user) => (
+          <div key={user.id}>
+            <h2>{user.email}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
